ab424c2738f88b5955e75a74a5628a1b779b6b6c
yee2542 <yee2542gtl@gmail.com>
2021-03-13 16:12:38 +0700

    Add router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import Router from "./router";
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
