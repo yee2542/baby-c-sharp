a91cc5743a02c6c02ca1abc57fc36dec8dc8cac4
yee2542 <yee2542gtl@gmail.com>
2021-04-03 17:20:01 +0700

    refactor

--- frontend/src/components/Layouts/Main/index.tsx
+++ frontend/src/components/Layouts/Main/index.tsx
@@ -7,14 +7,32 @@ import Tags from "../../Tags";
 const SAMPLE_TAGS = ["_id", "createAt", "gender", "updateAt", "activate"];
 const SAMPLE_DATA = `
 [
-    {"_id": "12hd34a5", "createAt: ${Date.now().toString()}, "gender": "M", "activate": true}
+    {
+        "_id": "12hd34a5", 
+        "createAt: ${Date.now().toString()}, 
+        "gender": "M", 
+        "activate": true
+    }
 ]
 `;
 
-const SearchLayout = () => (
+const SearchLayout: React.FC = () => (
   <Row justify="center">
     <Col span={24}>
       <LinkBar />
+    </Col>
+  </Row>
+);
+
+const MetaLayout: React.FC = () => (
+  <Row justify="space-between">
+    <Col span={12}>
+      <Divider orientation="left">Keys</Divider>
+      <Tags tags={SAMPLE_TAGS} />
+    </Col>
+    <Col span={12}>
+      <Divider orientation="left">Sample Data</Divider>
+      <SampleData language="json" data={SAMPLE_DATA} />
     </Col>
   </Row>
 );
@@ -23,17 +41,7 @@ const MainLayout: React.FC = (props) => {
   return (
     <div style={{ padding: 14 }}>
       <SearchLayout />
-
-      <Row justify="space-between">
-        <Col span={12}>
-          <Divider orientation="left">Keys</Divider>
-          <Tags tags={SAMPLE_TAGS} />
-        </Col>
-        <Col span={12}>
-          <Divider orientation="left">Sample Data</Divider>
-          <SampleData language="json" data={SAMPLE_DATA} />
-        </Col>
-      </Row>
+      <MetaLayout />
       {props.children}
     </div>
   );
