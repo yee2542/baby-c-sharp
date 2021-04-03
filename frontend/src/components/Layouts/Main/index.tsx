bc26294b2b23160986c2893a3f132856db2f89a5
yee2542 <yee2542gtl@gmail.com>
2021-04-03 16:59:14 +0700

    implement sample data layout

--- frontend/src/components/Layouts/Main/index.tsx
+++ frontend/src/components/Layouts/Main/index.tsx
@@ -1,10 +1,13 @@
-import { Col, Row } from "antd";
+import { Col, Divider, Row } from "antd";
 import React from "react";
 import { LinkBar } from "../../LinkBar";
+import Tags from "../../Tags";
+
+const SAMPLE_TAGS = ["_id", "createAt", "gender", "updateAt"];
 
 const SearchLayout = () => (
   <Row justify="center">
-    <Col span={20}>
+    <Col span={24}>
       <LinkBar />
     </Col>
   </Row>
@@ -14,6 +17,16 @@ const MainLayout: React.FC = (props) => {
   return (
     <div style={{ padding: 14 }}>
       <SearchLayout />
+
+      <Row justify="space-between">
+        <Col span={12}>
+          <Divider orientation="left">Keys</Divider>
+          <Tags tags={SAMPLE_TAGS} />
+        </Col>
+        <Col span={12}>
+          <Divider orientation="left">Sample Data</Divider>
+        </Col>
+      </Row>
       {props.children}
     </div>
   );
