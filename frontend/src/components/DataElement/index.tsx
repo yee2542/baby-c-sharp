1a69c6c18b6703ac8f1b3980884ee73c9466fdd2
yee2542 <yee2542gtl@gmail.com>
2021-04-16 22:35:59 +0700

    add data element

--- frontend/src/components/DataElement/index.tsx
+++ frontend/src/components/DataElement/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Highlight from "react-highlight";
+import styled from "styled-components";
 
-const DataElement = () => {
-  return <div></div>;
+const Card = styled.div`
+  width: 100%;
+  min-height: 115px;
+  border: 1px #d9d9d9 solid;
+`;
+
+const DataElement: React.FC<{ data?: Object }> = (props) => {
+  const format = JSON.stringify(props.data, null, 2);
+  return (
+    <Card>
+      <Highlight innerHTML={false}>{`${format}`}</Highlight>
+    </Card>
+  );
 };
 
 export default DataElement;
