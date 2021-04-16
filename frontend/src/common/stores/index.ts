7805fdc1955c5713fc1e711a49531a3b73c4bb89
yee2542 <yee2542gtl@gmail.com>
2021-04-16 23:04:52 +0700

    register data reducer

--- frontend/src/common/stores/index.ts
+++ frontend/src/common/stores/index.ts
@@ -1,6 +1,9 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { dataReducer } from "./reducers/data/data.slice";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  dataReducer,
+});
 export const store = configureStore({
   reducer,
 });
