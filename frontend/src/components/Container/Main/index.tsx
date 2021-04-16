3d142bb57671a4bc7f83905535d60763fce72ec6
yee2542 <yee2542gtl@gmail.com>
2021-04-16 23:30:26 +0700

    bind data reducer

--- frontend/src/components/Container/Main/index.tsx
+++ frontend/src/components/Container/Main/index.tsx
@@ -1,55 +1,98 @@
 import { Col, Divider, Row } from "antd";
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
-  SAMPLE_DATA,
   SAMPLE_ELEMENT,
   SAMPLE_TAGS,
 } from "../../../common/constants/mock.data";
+import { Store } from "../../../common/stores";
+import { dataActions } from "../../../common/stores/reducers/data/data.slice";
 import DataElement from "../../DataElement";
 import { LinkBar } from "../../LinkBar";
+import Loading from "../../Loading";
 import SampleData from "../../SampleData";
 import SearchBar from "../../SearchBar";
 import Tags from "../../Tags";
 
-const GetContainer: React.FC = () => (
-  <Row justify="center">
-    <Col span={24}>
-      <LinkBar />
-    </Col>
-  </Row>
-);
+const GetContainer: React.FC = () => {
+  return (
+    <Row justify="center">
+      <Col span={24}>
+        <LinkBar />
+      </Col>
+    </Row>
+  );
+};
 
-const MetaContainer: React.FC = () => (
-  <Row justify="space-between">
-    <Col span={12}>
-      <Divider orientation="left">Keys</Divider>
-      <Tags tags={SAMPLE_TAGS} />
-    </Col>
-    <Col span={12}>
-      <Divider orientation="left">Sample Data</Divider>
-      <SampleData language="json" data={SAMPLE_DATA} />
-    </Col>
-  </Row>
-);
+const MetaContainer: React.FC = () => {
+  const loading = useSelector((s: Store) => s.dataReducer.loading);
+  const tags = useSelector((s: Store) => s.dataReducer.keys);
+  const sample = useSelector((s: Store) => s.dataReducer.sample);
+  return (
+    <Row justify="space-between">
+      {loading && <Loading />}
+      {!loading && (
+        <>
+          <Col span={12}>
+            <Divider orientation="left">Keys</Divider>
+            <Tags tags={tags} />
+          </Col>
+          <Col span={12}>
+            <Divider orientation="left">Sample Data</Divider>
+            <SampleData
+              language="json"
+              data={JSON.stringify(sample, null, 2)}
+            />
+          </Col>
+        </>
+      )}
+    </Row>
+  );
+};
 
-const DataContainer: React.FC = () => (
-  <Row gutter={[16, 16]}>
-    <Divider orientation="left">Results</Divider>
-    <Col span={24}>
-      <SearchBar />
-    </Col>
+const DataContainer: React.FC = () => {
+  const loading = useSelector((s: Store) => s.dataReducer.loading);
+  const dataElement = useSelector((s: Store) => s.dataReducer.data);
+  return (
+    <Row gutter={[16, 16]}>
+      <Divider orientation="left">Results</Divider>
+      <Col span={24}>
+        <SearchBar />
+      </Col>
 
-    {Array(10)
-      .fill(0)
-      .map(() => (
-        <Col key={Math.random().toString()} xs={24} sm={8} md={6} lg={4}>
-          <DataElement data={SAMPLE_ELEMENT} />
-        </Col>
-      ))}
-  </Row>
-);
+      {loading && <Loading />}
+
+      {!loading &&
+        dataElement.map(() => (
+          <Col key={Math.random().toString()} xs={24} sm={8} md={6} lg={4}>
+            <DataElement data={SAMPLE_ELEMENT} />
+          </Col>
+        ))}
+    </Row>
+  );
+};
 
 const MainContainer: React.FC = (props) => {
+  const dispatch = useDispatch();
+
+  // first mount
+  useEffect(() => {
+    dispatch(dataActions.init());
+    dispatch(dataActions.fetching());
+    const mockElement = Array(10).fill(SAMPLE_ELEMENT);
+    setTimeout(
+      () =>
+        dispatch(
+          dataActions.load({
+            data: mockElement,
+            sample: SAMPLE_ELEMENT,
+            keys: SAMPLE_TAGS,
+          })
+        ),
+      1500
+    );
+  }, [dispatch]);
+
   return (
     <div style={{ padding: 14 }}>
       <GetContainer />
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import MainContainer from "../components/Container/Main";
 
 const IndexPage: React.FC = () => {
-  return <MainContainer>index</MainContainer>;
+  return <MainContainer />;
 };
 
 export default IndexPage;
